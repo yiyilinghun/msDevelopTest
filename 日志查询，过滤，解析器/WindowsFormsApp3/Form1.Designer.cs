5496dc6d5e4832e8e057b68cbe334fef339a6515
ms <200866850@qq.com>
2017-07-21 10:49:36 +0800

    递交项目

---
+++ 日志查询，过滤，解析器/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.B_获取ID_名字对应表 = new System.Windows.Forms.Button();
+            this.B_填充名字ID记录到LOG = new System.Windows.Forms.Button();
+            this.B_过滤日志 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // B_获取ID_名字对应表
+            // 
+            this.B_获取ID_名字对应表.Location = new System.Drawing.Point(343, 208);
+            this.B_获取ID_名字对应表.Name = "B_获取ID_名字对应表";
+            this.B_获取ID_名字对应表.Size = new System.Drawing.Size(225, 23);
+            this.B_获取ID_名字对应表.TabIndex = 0;
+            this.B_获取ID_名字对应表.Text = "获取ID_名字对应表";
+            this.B_获取ID_名字对应表.UseVisualStyleBackColor = true;
+            this.B_获取ID_名字对应表.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // B_填充名字ID记录到LOG
+            // 
+            this.B_填充名字ID记录到LOG.Location = new System.Drawing.Point(343, 237);
+            this.B_填充名字ID记录到LOG.Name = "B_填充名字ID记录到LOG";
+            this.B_填充名字ID记录到LOG.Size = new System.Drawing.Size(225, 23);
+            this.B_填充名字ID记录到LOG.TabIndex = 1;
+            this.B_填充名字ID记录到LOG.Text = "填充名字ID记录到LOG";
+            this.B_填充名字ID记录到LOG.UseVisualStyleBackColor = true;
+            this.B_填充名字ID记录到LOG.Click += new System.EventHandler(this.B_填充名字ID记录到LOG_Click);
+            // 
+            // B_过滤日志
+            // 
+            this.B_过滤日志.Location = new System.Drawing.Point(343, 181);
+            this.B_过滤日志.Name = "B_过滤日志";
+            this.B_过滤日志.Size = new System.Drawing.Size(225, 23);
+            this.B_过滤日志.TabIndex = 2;
+            this.B_过滤日志.Text = "过滤日志";
+            this.B_过滤日志.UseVisualStyleBackColor = true;
+            this.B_过滤日志.Click += new System.EventHandler(this.B_过滤日志_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 384);
+            this.Controls.Add(this.B_过滤日志);
+            this.Controls.Add(this.B_填充名字ID记录到LOG);
+            this.Controls.Add(this.B_获取ID_名字对应表);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button B_获取ID_名字对应表;
+        private System.Windows.Forms.Button B_填充名字ID记录到LOG;
+        private System.Windows.Forms.Button B_过滤日志;
+    }
+}
+
---
+++ 日志查询，过滤，解析器/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void SelectKeyLog(String xFile)
+        {
+            List<String> xListResult = new List<String>();
+            List<String> xListKey = new List<String>();
+            xListKey.Add(@"RoomPlayerResult");
+            xListKey.Add(@"MultiBattleSpace::LeaveSpace");
+            xListKey.Add(@"RoomPlayerResult");
+            xListKey.Add(@"time out quit");
+            xListKey.Add(@"pTeam m_kList.erase(it++);");
+            xListKey.Add(@"TeamManager::");
+
+            var xListLines = File.ReadAllLines(xFile);
+
+            foreach (String xLine in xListLines)
+            {
+                foreach (String xKey in xListKey)
+                {
+                    if (xLine.IndexOf(xKey) > 0)
+                    {
+                        xListResult.Add(xLine);
+                        this.Text = String.Format("已经找到[{0}]", xListResult.Count);
+                        break;
+                    }
+                }
+            }
+
+            File.WriteAllLines(xFile + ".log", xListResult);
+        }
+
+
+        Dictionary<Int64, String> m_DictResult = new Dictionary<Int64, String>();
+        void SelectID_NAME_TO_DICT(String xFile)
+        {
+            m_DictResult.Clear();
+            //List<String> xListKey = new List<String>();
+            //xListKey.Add(@"RoomPlayerResult");
+
+            var xListLines = File.ReadAllLines(xFile);
+            foreach (String xLine in xListLines)
+            {
+                Int32 xIndex = xLine.IndexOf("RoomPlayerResult");
+                if (xIndex > 0)
+                {
+                    String xSubString = xLine.Substring(xIndex);
+                    xIndex = xSubString.IndexOf(",ID");
+                    var xTempList = xSubString.Split(',');
+                    xTempList[1] = xTempList[1].Substring(2);
+                    xTempList[2] = xTempList[2];
+
+                    Int64 xID = Int64.Parse(xTempList[1]);
+                    if (!m_DictResult.ContainsKey(xID))
+                    {
+                        m_DictResult.Add(xID, xTempList[2]);
+                        this.Text = String.Format("已解析[{0}]个ID_NAME", m_DictResult.Count);
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //SelectKeyLog(@"C:\Users\Administrator\Desktop\大主宰，roomlog\room\log_20170719101906\ID_NAME_DICT.log");
+            SelectID_NAME_TO_DICT(@"C:\Users\Administrator\Desktop\大主宰，roomlog\room\log_20170719101906\ID_NAME_DICT.log");
+            MessageBox.Show("拉取名字ID对应表完成");
+        }
+
+
+
+
+
+
+        void FillIDNameToLog(String xFile)
+        {
+            String xAllLogs = File.ReadAllText(xFile);
+            foreach (var xID_Name in m_DictResult)
+            {
+                xAllLogs = xAllLogs.Replace(xID_Name.Key.ToString(), String.Format("{0}{1}", xID_Name.Value, xID_Name.Key));
+            }
+
+            File.WriteAllText(xFile + ".log", xAllLogs);
+        }
+
+        private void B_填充名字ID记录到LOG_Click(object sender, EventArgs e)
+        {
+            FillIDNameToLog(@"C:\Users\Administrator\Desktop\大主宰，roomlog\room\log_20170719101906\room.2017-07-18.log.log");
+            FillIDNameToLog(@"C:\Users\Administrator\Desktop\大主宰，roomlog\room\log_20170719101906\room.log.log.log");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void B_过滤日志_Click(object sender, EventArgs e)
+        {
+            foreach (String xFile in Directory.EnumerateFiles(@"C:\Users\Administrator\Desktop\dzz_log"))
+            {
+                SelectKeyLog(xFile);
+            }
+        }
+    }
+}
+
+

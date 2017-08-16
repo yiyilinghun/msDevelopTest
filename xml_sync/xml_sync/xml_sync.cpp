f8699b634e6fe01658b2a1e9e5d795e928b30be9
ms <200866850@qq.com>
2017-08-16 21:22:57 +0800

    只剩最后一步,行记录回归xml

--- xml_sync/xml_sync/xml_sync.cpp
+++ xml_sync/xml_sync/xml_sync.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <map>
 #include <set>
 #include <vector>
@@ -8,10 +9,12 @@
 
 enum MergeType
 {
+    MT_NON, // 未变
     MT_ADD, // 增加
-    MT_DEL, // 删除
+    //MT_DEL, // 删除
     MT_CHG, // 修改
     MT_MOV, // 移动
+    MT_END, // 结束
 };
 
 struct MergeInfo
@@ -129,20 +132,41 @@ int main()
         }
     }
 
+    FILE* xFile = fopen("d:\\1.txt", "w+");
+    for (Int32 i = 0; i < XmlInfoSrc.m_List_LineText.GetCount(); i++)
+    {
+        fprintf(xFile, "%s\n", XmlInfoSrc.m_List_LineText[i].c_str());
+    }
+    fclose(xFile);
+
+
+    xFile = fopen("d:\\2.txt", "w+");
+    for (Int32 i = 0; i < XmlInfoTar.m_List_LineText.GetCount(); i++)
+    {
+        fprintf(xFile, "%s\n", XmlInfoTar.m_List_LineText[i].c_str());
+    }
+    fclose(xFile);
     //printf(GETSTRMD5(XmlVector1[0].c_str()).c_str());
     //auto x1 = XmlInfoSrc.m_Dict_Md5_Text["0347f7e68541d577571b2f57b794e4f2"];
     //auto x2 = XmlInfoTar.m_Dict_Md5_Text["0347f7e68541d577571b2f57b794e4f2"];
 
 
+    MergeInfo xMergeInfo;
     MsList<MergeInfo> xListMergeLines;
     for (Int32 xTarLine = 0; xTarLine < XmlInfoTar.m_List_LineText.GetCount(); xTarLine++)
     {
-        MergeInfo xMergeInfo;
         mstr& xTarItem = XmlInfoTar.m_List_LineText[xTarLine];
         Int32 xSrcLine = XmlInfoSrc.m_List_LineText.IndexOf(xTarItem);
         if (xSrcLine >= 0)
         {
-            xMergeInfo.m_Type = MergeType::MT_MOV;
+            if (xTarLine == xSrcLine)
+            {
+                xMergeInfo.m_Type = MergeType::MT_NON;
+            }
+            else
+            {
+                xMergeInfo.m_Type = MergeType::MT_MOV;
+            }
             xMergeInfo.MergeData.m_SrcLine = xSrcLine;
             xListMergeLines.Add(xMergeInfo);
         }
@@ -164,42 +188,43 @@ int main()
             }
         }
     }
+    xMergeInfo.m_Type = MergeType::MT_END;
+    xListMergeLines.Add(xMergeInfo);
 
 
-    FILE* xFile = fopen("d:\\up.txt", "w+");
+    xFile = fopen("d:\\up.txt", "w+");
     for (Int32 i = 0; i < xListMergeLines.GetCount(); i++)
     {
         switch (xListMergeLines[i].m_Type)
         {
+            // 未变
+            case MergeType::MT_NON:
+            {
+                fprintf(xFile, "\n");
+            }break;
+
             // 增加
             case MergeType::MT_ADD:
             {
-                fprintf(xFile, "A\t%s\n", xListMergeLines[i].MergeData.m_AddValue.c_str());
-            }break;
-
-            // 删除
-            case MergeType::MT_DEL:
-            {
-                fprintf(xFile, "D\n");
+                fprintf(xFile, "A%s\n", xListMergeLines[i].MergeData.m_AddValue.c_str());
             }break;
 
             // 修改
             case MergeType::MT_CHG:
             {
-                fprintf(xFile, "C%d\t%s\n", xListMergeLines[i].MergeData.m_SrcLineNewValue.first, xListMergeLines[i].MergeData.m_SrcLineNewValue.second.c_str());
+                fprintf(xFile, "C%d=%s\n", xListMergeLines[i].MergeData.m_SrcLineNewValue.first, xListMergeLines[i].MergeData.m_SrcLineNewValue.second.c_str());
             }break;
 
             // 移动
             case MergeType::MT_MOV:
             {
-                if (i == xListMergeLines[i].MergeData.m_SrcLine)
-                {
-                    fprintf(xFile, "-\n");
-                }
-                else
-                {
-                    fprintf(xFile, "M%d\n", xListMergeLines[i].MergeData.m_SrcLine);
-                }
+                fprintf(xFile, "M%d\n", xListMergeLines[i].MergeData.m_SrcLine);
+            }break;
+
+            // 结束
+            case MergeType::MT_END:
+            {
+                fprintf(xFile, "E");
             }break;
         }
     }
@@ -207,21 +232,86 @@ int main()
 
 
 
-
-    xFile = fopen("d:\\1.txt", "w+");
-    for (Int32 i = 0; i < XmlInfoSrc.m_List_LineText.GetCount(); i++)
+    xListMergeLines.Clear();
+    FILE* xOutFile = fopen("d:\\out.txt", "w+");
+    std::fstream xRead("d:\\up.txt", std::ios::in);
+    Int32 xOutIndex = 0;
+    while (!xRead.eof())
     {
-        fprintf(xFile, "%s\n", XmlInfoSrc.m_List_LineText[i].c_str());
+        Char szBuff[2048];
+        xRead.getline(szBuff, 2048);
+        Char xFlag = -1;
+        Char szText[2048];
+        if (strlen(szBuff) == 0)
+        {
+            if (xOutIndex < XmlInfoSrc.m_List_LineText.GetCount())
+            {
+                fprintf(xOutFile, "%s\n", XmlInfoSrc.m_List_LineText[xOutIndex].c_str());
+            }
+        }
+        else
+        {
+            sscanf(szBuff, "%c%s\n", &xFlag, szText);
+        }
+        switch (xFlag)
+        {
+            //// 未变
+            //case '':
+            //{
+            //    fprintf(xOutFile, "%s\n", XmlInfoSrc.m_List_LineText[xOutIndex].c_str());
+            //}break;
+
+            // 增加
+            case 'A':
+            {
+                fprintf(xFile, "%s\n", szText);
+            }break;
+
+            // 修改
+            case 'C':
+            {
+                Int32 xSrcLine;
+                sscanf(szBuff, "C%d=%s\n", &xSrcLine, szText);
+                fprintf(xFile, "%s=%s\n", XmlInfoSrc.m_List_LineName[xSrcLine].c_str(), szText);
+            }break;
+
+            // 移动
+            case 'M':
+            {
+                Int32 xSrcLine;
+                sscanf(szBuff, "M%d\n", &xSrcLine);
+                fprintf(xFile, "%s\n", XmlInfoSrc.m_List_LineText[xSrcLine].c_str());
+            }break;
+
+            // 结束
+            case 'E':
+            {
+                xOutIndex = -1;
+            }break;
+        }
+        if (xOutIndex == -1)
+        {
+            break;
+        }
+        else
+        {
+            xOutIndex++;
+        }
     }
     fclose(xFile);
+    fclose(xOutFile);
 
 
-    xFile = fopen("d:\\2.txt", "w+");
-    for (Int32 i = 0; i < XmlInfoTar.m_List_LineText.GetCount(); i++)
-    {
-        fprintf(xFile, "%s\n", XmlInfoTar.m_List_LineText[i].c_str());
-    }
-    fclose(xFile);
+
+
+
+
+
+
+
+
+
+
 
 
 
